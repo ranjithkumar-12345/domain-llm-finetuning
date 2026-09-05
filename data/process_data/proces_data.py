b45c7f3ab18749a4f9bad60e65508a8050487f7e
ranjith <ranjithkumar180205@gmail.com>
2026-09-05 22:35:59 +0530

    updated file path

--- data/process_data/proces_data.py
+++ data/process_data/proces_data.py
@@ -1,7 +1,7 @@
 import os
 import json
 
-os.makedirs("/content/domain-llm-finetuning/data/processed", exist_ok=True)
+os.makedirs("/content/domain-llm-finetuning/data/process_data", exist_ok=True)
 
 devops_dataset = [
     {
@@ -54,7 +54,7 @@ devops_dataset = [
     }
 ]
 
-file_path = "/content/domain-llm-finetuning/data/processed/train.json"
+file_path = "/content/domain-llm-finetuning/data/process_data/process_data.py"
 
 with open(file_path, "w", encoding="utf-8") as f:
     json.dump(devops_dataset, f, indent=2)
